9fbd433ce605b887e7abd8ccb28fe884a1a9d07e
Jason Li <124201063+Jleeluit@users.noreply.github.com>
2023-04-20 11:14:10 -0400

    Update LambdaSQS.py

--- LambdaSQS.py
+++ LambdaSQS.py
@@ -20,5 +20,5 @@ def lambda_handler(event, context):
     )
     return {
         'statusCode': 200,
-        'body': json.dumps('Message sent to queue')
+        'body': json.dumps('Message in a bottle sent to SQSqueue :)')
     }
